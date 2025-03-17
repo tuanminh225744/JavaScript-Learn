e876e3f9d6a49d11e0d47366cb7eafd153c19f0d
Phạm Tuấn Minh <tuanminh100217@gmail.com>
2025-03-17 20:28:20 +0700

    commit

---
+++ main.js
@@ -0,0 +1,25 @@
+// function showDialog() {
+//     alert("hi xin chao cac ban")
+// }
+
+// showDialog();
+
+
+
+// function writeLog(log) {
+//     console.log(log)
+// }
+
+// writeLog('hello')
+
+
+// var isConfirm = confirm("ok");
+// console.log(isConfirm);
+
+
+// var fisrtName="minh"
+// var lastName="pham"
+// console.log(`toi la ${fisrtName} ${lastName}`);
+
+var fullName="Minh";
+console.log(fullName.charAt(0));
