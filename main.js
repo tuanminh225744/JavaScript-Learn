6ba9c216cf2bedf48bba0dc7046e281759a61950
Phạm Tuấn Minh <tuanminh100217@gmail.com>
2025-03-31 08:41:52 +0700

    commit

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // function showDialog() {
-//     alert("hi xin chao cac ban")
+//     alert('hi xin chao cac ban')
 // }
 
 // showDialog();
@@ -13,28 +13,28 @@
 // writeLog('hello')
 
 
-// var isConfirm = confirm("ok");
+// var isConfirm = confirm('ok');
 // console.log(isConfirm);
 
 
-// var fisrtName="minh"
-// var lastName="pham"
+// var fisrtName='minh'
+// var lastName='pham'
 // console.log(`toi la ${fisrtName} ${lastName}`);
 
-// var fullName="Minh";
+// var fullName='Minh';
 // console.log(fullName.charAt(0));
 
 
 // var myInfo={
-//     name: "minh",
+//     name: 'minh',
 //     age: 18,
-//     diachi: "hanoi",
+//     diachi: 'hanoi',
 //     getName: function(){
 //         return this.name;
 //     }
 // }
-// var myKey="name"
-// myInfo.email="tuanminh100217@gmail.com"
+// var myKey='name'
+// myInfo.email='tuanminh100217@gmail.com'
 // delete myInfo.age
 // console.log(myInfo.getName())
 
@@ -44,8 +44,8 @@
 //     this.lastName=lastName;
 //     this.avatar=avatar;
 // }
-// User.prototype.className="F8"
-// var author= new User("Minh","Pham","avatar")
+// User.prototype.className='F8'
+// var author= new User('Minh','Pham','avatar')
 // console.log(author)
 
 
@@ -88,23 +88,110 @@
 
 
 // function myFunction(param){
-//     if(typeof param==="function"){
-//         param("hoc lap trinh");
+//     if(typeof param==='function'){
+//         param('hoc lap trinh');
 //     }
 // }
 
 // function myCallBack(value){
-//     console.log("value: ",value)
+//     console.log('value: ',value)
 // }
 
 // myFunction(myCallBack);
 
 
-// var courses=["1a","2a","3a"]
+// var courses=['1a','2a','3a']
 
 // courses.forEach(function(courses,index,array){
 //     console.log(courses,index,array)
 // },)
 
-var headingNode=document.querySelectorAll('.box li')
-console.log(headingNode)
+// var headingNode=document.querySelectorAll('.box li')
+// console.log(headingNode)
+
+
+// lay va gan atribute cho element***************************************************
+// var headingElement=document.querySelector('h1')
+// headingElement.className='heading'  
+// headingElement.title='heading'
+// headingElement.id='heading'
+// headingElement.setAttribute('href','heading2')
+// headingElement.classList.add('heading3')
+// console.log(headingElement.getAttribute('class'))
+// console.log(headingElement.id)
+// console.log(headingElement);
+
+
+// lay va gan text cho element*******************************************************
+// var headingElement=document.querySelector('h1')
+// // headingElement.textContent='minh dep trai'
+// console.log(headingElement.innerText)
+// console.log(headingElement.textContent)
+// // inner text the hien ra nhung gi minh thuc su thay tren trinh duyet
+// // textContent the hien nhung gi thuc hien trong text note
+
+
+// them mot element vao mot element khac*********************************************
+// var boxElement=document.querySelector('.box')
+// // inner them mot element vao trong box
+// boxElement.innerHTML= '<h1 title="minh">Heading</h1>'
+// // inner thay the element vao box
+// boxElement.outerHTML= '<h1 title="minh">Heading</h1>'
+// // goi ra thanh phan trong box
+// console.log(boxElement.innerHTML)
+// // goi ra ca box
+// console.log(boxElement.outerHTML)
+
+
+// css DOM**************************************************************************
+// var boxElement=document.querySelector('.box')
+// // boxElement.style.width='120px'
+// // boxElement.style.height='120px'
+// // boxElement.style.backgroundColor='red'
+// Object.assign(boxElement.style,{
+//     width:'120px',
+//     height:'120px',
+//     backgroundColor:'red',
+//     opacity:0.5
+// })
+// console.log(boxElement.style.opacity)
+
+
+//classList*************************************************************************
+// var boxElement=document.querySelector('.box')
+// console.log(boxElement.classList.length)
+// console.log(boxElement.classList[1])
+// console.log(boxElement.classList.value)
+// boxElement.classList.add('box3','box4')
+// console.log(boxElement.classList.contains('box2'))
+// boxElement.classList.remove('box')
+// boxElement.classList.toggle('box')
+// console.log(boxElement.classList)
+
+
+// DOM-event************************************************************************
+// var h1Elements=document.querySelectorAll('h1')
+// for(var i=0;i<h1Elements.length;i++){
+//     h1Elements[i].onclick=function(e){
+//         console.log(e.target) 
+//     }
+// }
+
+
+// DOM-event2***********************************************************************
+// var inputElement=document.querySelector('input[type="text"]')
+// inputElement.onchange=function(e){
+//     console.log(e.target.value)
+// }
+// inputElement.oninput=function(e){
+//     console.log(e.target.value)
+// }
+// var inputElement=document.querySelector('input[type="checkbox"]')
+// inputElement.onchange=function(e){
+//     console.log(e.target.checked)
+// }
+var inputElement=document.querySelector('select')
+inputElement.onchange=function(e){
+    console.log(e.target.value)
+}
+
